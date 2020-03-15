4d1b6afdbeebcb3547bdea6dfa3f863f6a200a8a
Viacheslav Dobromyslov <viacheslav@dobromyslov.ru>
2020-03-15 17:09:34 +1000

    Added test for DateTime.

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,8 +1,15 @@
-import {parseRussianDate} from '../src';
+import {parseRussianDate, parseRussianDateTime} from '../src';
 
 describe('parseRussianDate', () => {
     it('parses correctly', async () => {
         const testDate = '14.12.2020';
-        expect(parseRussianDate(testDate).toFormat("dd.MM.yyyy")).toEqual(testDate);
+        expect(parseRussianDate(testDate).toFormat('dd.MM.yyyy')).toEqual(testDate);
     });
+});
+
+describe('parseRussianDateTime', () => {
+  it('parses correctly', async () => {
+    const testDateTime = '28.02.2020 11:20:44';
+    expect(parseRussianDateTime(testDateTime)?.toFormat('dd.MM.yyyy HH:mm:ss')).toEqual(testDateTime);
+  })
 });
